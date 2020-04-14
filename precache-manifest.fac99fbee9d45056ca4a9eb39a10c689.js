6a63b9a8d78ac9a221757a3782fa3a37816840ab
BhaveshVaru <bhavehs.varu97@gmail.com>
2020-04-14 14:21:26 +0530

    Updates

---
+++ precache-manifest.fac99fbee9d45056ca4a9eb39a10c689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02eada472bae1def9261ef6ee97aecc",
+    "url": "/reactapp-covid-19/index.html"
+  },
+  {
+    "revision": "6f89cac4d4c108c01c8d",
+    "url": "/reactapp-covid-19/static/css/main.ddaeaf1e.chunk.css"
+  },
+  {
+    "revision": "f0be28ebaabb0133fffb",
+    "url": "/reactapp-covid-19/static/js/2.2001c4f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp-covid-19/static/js/2.2001c4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f89cac4d4c108c01c8d",
+    "url": "/reactapp-covid-19/static/js/main.be600e3b.chunk.js"
+  },
+  {
+    "revision": "5335af18db2ba6bd9f75",
+    "url": "/reactapp-covid-19/static/js/runtime-main.2b4d5605.js"
+  }
+]);
